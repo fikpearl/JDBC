db74434620c420f15c36b94d1c958567b44970d2
fikpearl021 <fikpearl021@student.wethinkcode.co.za>
2023-01-12 13:56:54 +0200

    Just a few changes and comments

--- src/main/java/connect/jdbc/connectivity/JDBC.java
+++ src/main/java/connect/jdbc/connectivity/JDBC.java
@@ -7,27 +7,20 @@ public class JDBC {
 
     public static void main(String[] args) throws SQLException {
 
-        String url = "jdbc:sqlite:/home/fikilegqibitole/NEWDB";
-        String query = "select * from SBHotel";
-
-        /*try{
-            Class.forName("");
-        } catch (ClassNotFoundException e){
-            e.printStackTrace();
-        }*/
+        String url = "jdbc:sqlite:/home/fikilegqibitole/NEWDB"; //url for database,where all the data I want to print is
+        String query = "select * from SBHotel";   //What from the data do I want to print
 
         try {
-            Connection connect = DriverManager.getConnection(url);
+            Connection connect = DriverManager.getConnection(url); //Connecting Database
             Statement state = connect.createStatement();
-            ResultSet set = state.executeQuery(query);
+            ResultSet set = state.executeQuery(query); //used to execute query (enables printing what I want from the data)
 
-            while(set.next()){
-                String SBHotelData = "";
+            while(set.next()){                 //While loop to loop through each row/column
+                String SBHotelData = "";                  //of data and print out everything in each column (I Have 4 columns and 7 rows)
                 for(int i = 1; i <= 4;i++){
                     SBHotelData += set.getString(i) + ":";
                  }
                 System.out.println(SBHotelData);
-
             }
         } catch (SQLException e) {
             e.printStackTrace();
